dc63b3795b83fa085fc834cd579de15cd96205bf
Stéphane Depierrepont aka Toorop <toorop@toorop.fr>
2016-02-15 15:49:31 +0100

    base server

--- urbl.go
+++ urbl.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"log"
+	"net"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"syscall"
+	"time"
 
 	"github.com/boltdb/bolt"
 	"github.com/miekg/dns"
 )
+
+const bucket = "bl"
 
 var (
 	//DB Bolt connector
@@ -17,18 +24,116 @@ var (
 )
 
 func listenAndServe(ip, secret string) {
-
 	// Launch server
 	server := &dns.Server{Addr: ip + ":1053", Net: "udp"}
-
 	if secret != "" {
-		server.TsigSecret = map[string]string{"k": secret}
+		server.TsigSecret = map[string]string{"urbl.": secret}
 	}
-
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("Failed to launch server: %s", err.Error())
 	}
+}
 
+// return formated key (domain_type)
+func formatKey(domain string, rtype uint16) (key string, err error) {
+	domain = strings.ToLower(domain)
+	if _, ok := dns.IsDomainName(domain); !ok {
+		return "", errors.New(domain + " is not a valid domain")
+	}
+	key = domain + "_" + strconv.Itoa(int(rtype))
+	return
+}
+
+// Returns a Record (dns.RR) from DB
+func getRecord(domain string, rtype uint16) (rr dns.RR, err error) {
+	var key string
+	var value []byte
+	if key, err = formatKey(domain, rtype); err != nil {
+		return
+	}
+	err = DB.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		value = b.Get([]byte(key))
+
+		if len(value) == 0 {
+			e := errors.New("Record not found, key:  " + key)
+			log.Println(e.Error())
+			return e
+		}
+		return nil
+	})
+	if err == nil {
+		rr, err = dns.NewRR(string(value))
+	}
+	return
+}
+
+// delete a record
+func deleteRecord(domain string, rtype uint16) (err error) {
+	var key string
+	if key, err = formatKey(domain, rtype); err != nil {
+		return
+	}
+	err = DB.Update(func(tx *bolt.Tx) error {
+		if err = tx.Bucket([]byte(bucket)).Delete([]byte(key)); err != nil {
+			return err
+		}
+		return nil
+	})
+	return
+}
+
+// save record
+func saveRecord(rr dns.RR) (err error) {
+	var key string
+	if key, err = formatKey(rr.Header().Name, rr.Header().Rrtype); err != nil {
+		return
+	}
+	err = DB.Update(func(tx *bolt.Tx) error {
+		if err := tx.Bucket([]byte(bucket)).Put([]byte(key), []byte(rr.String())); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	return err
+}
+
+// update record
+func updateRecord(r dns.RR, q *dns.Question) {
+
+	// record to update
+	var rr dns.RR
+	// IP of record
+	var ip net.IP
+
+	header := r.Header()
+	if _, ok := dns.IsDomainName(header.Name); ok {
+		if header.Class == dns.ClassANY && header.Rdlength == 0 { // Delete record
+			deleteRecord(header.Name, header.Rrtype)
+		} else { // Add record
+			rheader := dns.RR_Header{
+				Name:   header.Name,
+				Rrtype: header.Rrtype,
+				Class:  dns.ClassINET,
+				Ttl:    header.Ttl,
+			}
+
+			// IPv4 only
+			if a, ok := r.(*dns.A); ok {
+				rrr, err := getRecord(header.Name, header.Rrtype)
+				if err == nil {
+					rr = rrr.(*dns.A)
+				} else {
+					rr = new(dns.A)
+				}
+				ip = a.A
+				rr.(*dns.A).Hdr = rheader
+				rr.(*dns.A).A = ip
+				saveRecord(rr)
+			}
+		}
+	}
 }
 
 // base handler for dns server
@@ -37,14 +142,36 @@ func dnsHandler(w dns.ResponseWriter, request *dns.Msg) {
 	response.SetReply(request)
 	response.Compress = false
 
-	log.Println(request.Question)
-	for _, question := range request.Question {
-		log.Println(question.Name, question.Qtype)
-		log.Println(question.String())
+	switch request.Opcode {
+	case dns.OpcodeQuery:
+		for _, q := range response.Question {
+			if readRR, e := getRecord(q.Name, q.Qtype); e == nil {
+				rr := readRR.(dns.RR)
+				if rr.Header().Name == q.Name {
+					response.Answer = append(response.Answer, rr)
+				}
+			}
+		}
+	case dns.OpcodeUpdate:
+		if request.IsTsig() != nil && w.TsigStatus() == nil {
+			for _, question := range request.Question {
+				for _, rr := range request.Ns {
+					updateRecord(rr, &question)
+				}
+			}
+		} else {
+			log.Println("droping update without tsig or with bad sig")
+		}
+	}
 
+	if request.IsTsig() != nil {
+		if w.TsigStatus() == nil {
+			response.SetTsig(request.Extra[len(request.Extra)-1].(*dns.TSIG).Hdr.Name, dns.HmacMD5, 300, time.Now().Unix())
+		} else {
+			log.Println("Status: ", w.TsigStatus().Error())
+		}
 	}
 	w.WriteMsg(response)
-
 }
 
 // Main
@@ -68,6 +195,15 @@ func main() {
 		log.Fatalln(err)
 	}
 	defer DB.Close()
+
+	// create bucket if needed
+	err = DB.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(bucket))
+		return err
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	//dns handler
 	dns.HandleFunc(".", dnsHandler)
